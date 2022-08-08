fefe41941d8377352c91d0db41994ee62a68ab86
atrox39 <cofter11@gmail.com>
2022-08-08 19:11:44 +0000

    log

--- api/index.js
+++ api/index.js
@@ -1,25 +1,32 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
 
+const LOG_PATH = path.join(__dirname, 'data.log');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const webhook = (req, res, next) => {
-  const params = req.query;
-  const body = req.body;
-  const urlParams = req.params;
-  console.log(`Params: ${params}\nBody: ${body}\nUrlParams: ${urlParams}\n\n`);
+  const { query, body, params } = req.query;
+  console.log(`Params: ${query}\nBody: ${body}\nUrlParams: ${params}\n\n`);
   res.json({
-    params,
+    params: query,
     body,
-    urlParams,
+    urlParams: params,
   });
+  fs.writeFileSync(LOG_PATH, `Params: ${query}\nBody: ${body}\nUrlParams: ${params}\n\n`);
   next();
-}
+};
 
 app.get('/webhook', webhook);
 app.post('/webhook', webhook);
 app.put('/webhook', webhook);
 app.delete('/webhook', webhook);
+// Data
+app.get('/logs', (req, res) => {
+  const logs = fs.readFileSync(LOG_PATH);
+  res.send(logs);
+});
 
 app.listen(PORT, () => {
   console.log('Initialized');
